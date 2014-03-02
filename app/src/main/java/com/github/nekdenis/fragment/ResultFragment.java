6ab59d78d59a54c7e431a01fedccd664d20407dd
nekdenis <batman36@mail.ru>
2014-03-02 19:55:08 +0200

    Added plot of result

--- app/src/main/java/com/github/nekdenis/fragment/ResultFragment.java
+++ app/src/main/java/com/github/nekdenis/fragment/ResultFragment.java
@@ -8,9 +8,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import com.androidplot.xy.*;
 import com.github.nekdenis.R;
 import com.github.nekdenis.dto.ColorObj;
 import com.github.nekdenis.dto.ResultObj;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Fragments for showing and sharing results
@@ -18,6 +22,10 @@ import com.github.nekdenis.dto.ResultObj;
 public class ResultFragment extends Fragment {
 
     public static final String EXTRA_RESULT_OBJECT = "EXTRA_RESULT_OBJECT";
+    public static final String EXTRA_DATASET = "dataset";
+    public static final String EXTRA_RENDERER = "renderer";
+    public static final String EXTRA_CURRENT_SERIES = "current_series";
+    public static final String EXTRA_CURRENT_RENDERER = "current_renderer";
 
     private ResultObj resultObj;
 
@@ -27,6 +35,7 @@ public class ResultFragment extends Fragment {
     private TextView centerColorText;
     private Button shareButton;
 
+    private XYPlot plot;
 
     public static ResultFragment newInstance(ResultObj resultObj) {
         ResultFragment fragment = new ResultFragment();
@@ -47,6 +56,8 @@ public class ResultFragment extends Fragment {
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -57,11 +68,45 @@ public class ResultFragment extends Fragment {
         centerColorView = view.findViewById(R.id.result_center_color);
         rightColorView = view.findViewById(R.id.result_right_color);
         shareButton = (Button) view.findViewById(R.id.result_share_button);
+        plot = (XYPlot) view.findViewById(R.id.mySimpleXYPlot);
 
+        initPlot();
         fillView();
         initListeners();
-
         return view;
+    }
+
+    private void initPlot() {
+        List<Double> xs = new ArrayList<Double>();
+        List<Double> ys = new ArrayList<Double>();
+        for(ColorObj colorObj: resultObj.getModifiedColors()){
+            xs.add(colorObj.getA());
+            ys.add(colorObj.getB());
+        }
+        //add first again for full cirle
+        xs.add(xs.get(0));
+        ys.add(ys.get(0));
+
+        XYSeries series1 = new SimpleXYSeries(xs,ys,"");
+
+        LineAndPointFormatter series1Format = new LineAndPointFormatter();
+        series1Format.setPointLabelFormatter(new PointLabelFormatter());
+        series1Format.configure(getActivity(),
+                R.xml.line_point_formatter_with_plf1);
+        plot.addSeries(series1, series1Format);
+        plot.setRangeLabel("");
+        plot.setDomainLabel("");
+        plot.setTitle("");
+        plot.setTicksPerRangeLabel(3);
+        plot.getGraphWidget().setDomainLabelOrientation(-45);
+        plot.setDomainBoundaries(-128, 128, BoundaryMode.FIXED);
+        plot.setRangeBoundaries(-128, 128, BoundaryMode.FIXED);
+        plot.redraw();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 
     private void initListeners() {
